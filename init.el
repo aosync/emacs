c5aa674cddae69146b714958ab8556ad64c814d4
Alejandro Sior <aws@sior.sior.be>
2021-07-07 15:00:46 +0200

    added undo-tree

--- init.el
+++ init.el
@@ -47,7 +47,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(magit projectile ## auto-package-update)))
+ '(package-selected-packages '(undo-tree magit projectile ## auto-package-update)))
 
 (use-package auto-package-update
   :config
@@ -86,3 +86,6 @@
   ("C-c p" . projectile-command-map))
 
 (use-package magit)
+(use-package undo-tree
+  :config
+  (global-undo-tree-mode))
